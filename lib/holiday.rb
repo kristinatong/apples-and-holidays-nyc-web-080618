f0e1b87d7b99fcfa2a951e342f45a4373ae17fc5
Kristina Tong <kristina.tong@gmail.com>
2018-08-03 23:21:12 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,8 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season,holiday|
     puts "#{season.capitalize}:"
     holiday.each do |type,item|
-      hol = 
-      puts "  #{type.capitalize}: #{item.join(", ")}"
+      puts "  #{type.capitalize.humanize}: #{item.join(", ")}"
     end
   end
 end
