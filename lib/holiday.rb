633e24cb9ae3a0ffe85e0092960dcbf1d9ef1631
Kristina Tong <kristina.tong@gmail.com>
2018-08-03 23:14:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  array = []
+  array << holiday_hash[:winter].values
+  array.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -56,7 +58,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season,holiday|
+    puts season
+    holiday.each do |type,item|
+      puts "#{type}: #{item}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
